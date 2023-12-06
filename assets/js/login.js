537c7fd9f4b2d70cde03bcf66831f887750c42bc
lucazalcantara <lucasprestesmatos@hotmail.com>
2023-12-06 15:44:25 -0300

    alterações validação login e indexação de paginas

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,9 +1,60 @@
+/*
+const users = [
+
+    {
+        login: 'francine.usuarioteste@connectpet.com',
+        pass: 'francine'
+    },
+    {
+        login: 'ingrid.usuarioteste@connectpet.com',
+        pass: 'ingrid'
+    },
+    {
+        login: 'eliezer.usuarioteste@connectpet.com',
+        pass: 'eliezer'
+    },
+    {
+        login: 'maran.usuarioteste@connectpet.com',
+        pass: 'maran'
+    },
+    {
+        login: 'lucas.usuarioteste@connectpet.com',
+        pass: 'lucas'
+    }
+
+];
+
+let entrar = document.getElementById('btnEntrar');
+
+entrar.addEventListener('click', function logar(){
+
+    let emailUser = document.getElementById('email').value;
+    let senhaUser = document.getElementById('senha').value;
+    let validarLogin = false;
+
+    for(let i in users){
+
+        if(emailUser == users[i].login && senhaUser == users[i].pass){
+            validarLogin = true
+            break 
+        }
+    }
+    
+    if(validarLogin == true){
+        location.href = '../../HTML_final/inicio.html';
+    }else{
+        alert('usuário e senha incorretos')
+    }
+
+})
+
+*/
+
 function logar(){
     var email = document.getElementById('email').value;
     var senha = document.getElementById('senha').value;
 
-    if(email == 'usuarioteste@connectpet.com' && senha == 'lkjhgfdsa'){
-        alert('sucesso');
+    if(email == 'usuarioteste@connectpet.com' && senha == '54321'){
         location.href = "../../HTML_final/inicio.html"
     }else{
         alert('Usuário ou senha incorretos');
