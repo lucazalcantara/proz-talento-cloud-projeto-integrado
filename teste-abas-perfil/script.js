55cfd1822c8c734e0c02a61c7b9c39cddf721af1
lucazalcantara <lucasprestesmatos@hotmail.com>
2023-12-10 23:01:16 -0300

    abas da página: perfil (finalizadas)

--- teste-abas-perfil/script.js
+++ teste-abas-perfil/script.js
@@ -17,17 +17,17 @@
 
 // depois de finalizar, tente trocar o toggle para add para nao ficar trocando.
 
-var display = document.querySelectorAll('.perfil-conteudo');
+// var display = document.querySelectorAll('.perfil-conteudo');
 
-function selectLink(){
-    display.forEach((item)=> {
-        item.classList.remove('ativo')
-    })
-    this.classList.add('ativo')
-}
+// function selectLink(){
+//     display.forEach((item)=> {
+//         item.classList.remove('ativo')
+//     })
+//     this.classList.add('ativo')
+// }
 
-var ativador = document.querySelectorAll('.aba');
+// var ativador = document.querySelectorAll('.aba');
 
-ativador.forEach((item)=>
-    item.addEventListener('click', selectLink())
-)
+// ativador.forEach((item)=>
+//     item.addEventListener('click', selectLink())
+// )
