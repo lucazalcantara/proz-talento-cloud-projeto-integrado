922cc1bf128425b773f7adc250b04623fe0e1b93
lucazalcantara <lucasprestesmatos@hotmail.com>
2023-12-17 13:31:00 -0300

    Atualização chat

---
+++ assets/js/chat-teste.js
@@ -0,0 +1,9 @@
+let windowSection = document.querySelector(".chat-window");
+
+function showHideWindow(){
+    windowSection.classList.toggle("hide");
+}
+
+function minMaxWindow(){
+    windowSection.classList.toggle("minimize");
+}
